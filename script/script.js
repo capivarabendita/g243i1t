a7a5dd618157b37cc7d546c6c0030ccd6ff2ad30
capivarabendita <127324209+capivarabendita@users.noreply.github.com>
2024-04-08 08:58:15 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -7,23 +7,55 @@ function abrirraba(num) {
     }
     if (num == "1") {
         document.getElementById("btn1").disabled = true;
-        document.getElementById("letra_1").style.display = "block";
+        document.getElementById("aba1").style.display = "block";
     }
     if (num == "2") {
         document.getElementById("btn2").disabled = true;
-        document.getElementById("letra_2").style.display = "block";
+        document.getElementById("aba2").style.display = "block";
     }
     if (num == "3") {
         document.getElementById("btn3").disabled = true;
-        document.getElementById("letra_3").style.display = "block";
+        document.getElementById("aba3").style.display = "block";
     }
     if (num == "4") {
         document.getElementById("btn4").disabled = true;
-        document.getElementById("letra_4").style.display = "block";
+        document.getElementById("aba4").style.display = "block";
     }
     if (num == "5") {
         document.getElementById("btn5").disabled = true;
-        document.getElementById("letra_5").style.display = "block";
+        document.getElementById("aba5").style.display = "block";
     }
 
 }
+
+let contador = null;
+let inicio = 0;
+let fim = 0;
+
+
+function contagem(){
+    if(inicio != fim ){
+        document.getElementById("valor").innerHTML = inicio;
+   
+    if(inicio > fim){
+        inicio--
+    }
+    else{
+        inicio ++;
+    }
+    }
+
+    
+}
+
+function contar(){
+    inicio = document.getElementById("inicio").value;
+    fim = document.getElementById("fim").value;
+    if(inicio > fim)
+        inicio = Number(inicio) -1; 
+    
+    else fim = Number(fim) -2;
+    
+    contador = setInterval(contagem, 100);
+}
+
